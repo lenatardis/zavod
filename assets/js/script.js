3f611ce2d3e81884d79ff91e1a38946c057ca66d
lenatardis <olenaskuzmina@gmail.com>
2024-06-17 22:54:40 +0300

    Added some header and header menu styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
 $(function($) {
-    console.log('jquery');
+    $('header button').click(function() {
+        $('.header__menu').toggleClass('active');
+        $('.open-block').toggleClass('hide');
+        $('.close-block').toggleClass('show');
+    });
+
+    $('.header__dropdown-link').click(function(e){
+        e.preventDefault();
+        $(this).next('.header__dropdown').slideToggle(100);
+    })
 });
