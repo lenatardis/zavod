9b5f0e6b0063d359d3f759766aeff06d7e803a83
lenatardis <olenaskuzmina@gmail.com>
2024-06-18 22:33:49 +0300

    Added language dropdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,4 +9,26 @@ $(function($) {
         e.preventDefault();
         $(this).next('.header__dropdown').slideToggle(100);
     })
+
+    $('.lang-dropdown__main').click(function(e){
+        e.stopPropagation();
+        $(this).next('.lang-dropdown__options').slideToggle();
+        $(this).find('img').toggleClass('rotated-arr');
+    })
+
+    $('.lang-dropdown__options li').click(function(e){
+        e.stopPropagation();
+        let text = $(this).children('a').text();
+        let dropdownToggle =  $(this).parents('.lang-dropdown').find('.lang-dropdown__main');
+        dropdownToggle.find('.lang-dropdown__selected').text(text).next('img').removeClass('rotated-arr');
+        $(this).parent().slideUp();
+    })
+
+    $(document).on('click', function(e) {
+        let $dropdown = $('.lang-dropdown');
+        if (!$dropdown.is(e.target) && !$dropdown.has(e.target).length) {
+            $dropdown.find('.lang-dropdown__options').slideUp();
+            $dropdown.find('img').removeClass('rotated-arr');
+        }
+    });
 });
